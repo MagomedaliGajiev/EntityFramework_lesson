3f00e255b4a854e071dfca96b5a8f4bf5bbefb96
Magomedali Gajiev <mag198421@gmail.com>
2025-08-03 23:28:19 +0300

    Add project files.

---
+++ EntityFramework_lesson/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EntityFramework_lesson;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Phone> Phones { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = config.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
+}
---
+++ EntityFramework_lesson/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework_lesson;
+
+public class Program
+{
+    public static void Main()
+    {
+        var phone = new Phone()
+        {
+            Name = "Xiaomi Poco F4",
+            Price = 25000
+        };
+
+        using var dbContext = new ApplicationDbContext();
+        dbContext.Phones.Add(phone);
+        dbContext.SaveChanges();
+    }
+}
