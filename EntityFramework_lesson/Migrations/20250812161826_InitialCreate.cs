107068e3be28ef670661151dae8da528b0a30d49
Magomedali Gajiev <mag198421@gmail.com>
2025-08-12 19:21:32 +0300

    Добавлены и применены миграции

---
+++ EntityFramework_lesson/Migrations/20250812161826_InitialCreate.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFramework_lesson.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "apps",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    title = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_apps", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "phones",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    price = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    AppId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_phones", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_phones_apps_AppId",
+                        column: x => x.AppId,
+                        principalTable: "apps",
+                        principalColumn: "id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Guarantees",
+                columns: table => new
+                {
+                    phone_id = table.Column<int>(type: "int", nullable: false),
+                    months = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guarantees", x => x.phone_id);
+                    table.ForeignKey(
+                        name: "FK_Guarantees_phones_phone_id",
+                        column: x => x.phone_id,
+                        principalTable: "phones",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "software_updates",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    version = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    release_date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    PhoneId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_software_updates", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_software_updates_phones_PhoneId",
+                        column: x => x.PhoneId,
+                        principalTable: "phones",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_phones_AppId",
+                table: "phones",
+                column: "AppId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_software_updates_PhoneId",
+                table: "software_updates",
+                column: "PhoneId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Guarantees");
+
+            migrationBuilder.DropTable(
+                name: "software_updates");
+
+            migrationBuilder.DropTable(
+                name: "phones");
+
+            migrationBuilder.DropTable(
+                name: "apps");
+        }
+    }
+}
